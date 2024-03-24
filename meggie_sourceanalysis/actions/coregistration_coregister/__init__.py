96ae335fab57a6380a594824feb9b09b57d66407
teekuningas <erkkahe@gmail.com>
2024-03-24 13:43:07 +0200

    Refactor coregistration code

--- meggie_sourceanalysis/actions/coregistration_coregister/__init__.py
+++ meggie_sourceanalysis/actions/coregistration_coregister/__init__.py
@@ -1,20 +1,73 @@
 """ Contains create coregistration action handling.
 """
 
+import os
+import shutil
+import mne
+import logging
+
 from meggie.utilities.names import next_available_name
+from meggie.utilities.threading import threaded
 
 from meggie.mainwindow.dynamic import Action
 from meggie.mainwindow.dynamic import subject_action
 
 from meggie.utilities.dialogs.simpleDialogMain import SimpleDialog
 
-from meggie_sourceanalysis.actions.coregistration_coregister.controller.coregistration import (
-    coregister_default,
-)
+from meggie_sourceanalysis.datatypes.coregistration.coregistration import Coregistration
+from meggie.utilities.messaging import messagebox
 
 
 class Coregister(Action):
     """Opens mne coregistration utility."""
+
+    def coreg_utility_handler(self, subject, params):
+        name = params["name"]
+
+        coreg_dir = os.path.join(subject.coregistration_directory, name)
+        subjects_dir = os.path.join(coreg_dir, "subjects")
+
+        # remove existing files
+        try:
+            shutil.rmtree(coreg_dir)
+        except Exception:
+            pass
+
+        logging.getLogger("ui_logger").info("Fetching fsaverage..")
+
+        @threaded
+        def threaded_fetch():
+            return mne.datasets.fetch_fsaverage(verbose=False)
+
+        fs_dir = threaded_fetch(do_meanwhile=self.window.update_ui)
+
+        shutil.copytree(fs_dir, os.path.join(subjects_dir, "fsaverage"))
+
+        # Open the mne coregistration UI
+        open_message = (
+            "An external coregistration GUI is now opened. You should "
+            "fit the digization points to the scalp and then click save "
+            "on the right panel. The trans file should be saved as "
+            "`experiment_folder`/`subject_name`/coregistrations/"
+            "`coreg_name`/`subject_name`-trans.fif"
+        )
+        messagebox(self.window, open_message)
+
+        logging.getLogger("ui_logger").info("Opening coregistration utility.")
+        gui = mne.gui.coregistration(
+            inst=subject.raw_path, subject="fsaverage", subjects_dir=subjects_dir
+        )
+
+        def close_wrapper(*args, **kwargs):
+            if not gui._accept_close_event:
+                return
+            logging.getLogger("ui_logger").info("Coregistration utility closed.")
+            self.handler(subject, params)
+
+            self.experiment.save_experiment_settings()
+            self.window.initialize_ui()
+
+        gui._renderer._window_close_connect(close_wrapper)
 
     def run(self):
 
@@ -26,7 +79,7 @@ class Coregister(Action):
             self.experiment,
             self.window,
             coreg_name,
-            self.handler,
+            self.coreg_utility_handler,
             batching=False,
             title="Create coregistration",
         )
@@ -35,4 +88,50 @@ class Coregister(Action):
     @subject_action
     def handler(self, subject, params):
         """ """
-        coregister_default(self.experiment, self.window, subject, params["name"])
+
+        name = params["name"]
+
+        coreg_dir = os.path.join(subject.coregistration_directory, name)
+        subjects_dir = os.path.join(coreg_dir, "subjects")
+
+        if not os.path.exists(os.path.join(coreg_dir, subject.name + "-trans.fif")):
+            logging.getLogger("ui_logger").info(
+                "Coregistration utility was closed without saving or "
+                "the trans file was saved to a wrong location. "
+                "The trans file should be saved as "
+                "`experiment_folder`/`subject_name`/coregistrations/"
+                "`coreg_name`/`subject_name`-trans.fif"
+            )
+            shutil.rmtree(coreg_dir)
+            return
+
+        if not os.path.exists(os.path.join(subjects_dir, subject.name)):
+            shutil.copytree(
+                os.path.join(subjects_dir, "fsaverage"),
+                os.path.join(subjects_dir, subject.name),
+            )
+
+            for dirpath, _, fnames in reversed(
+                list(os.walk(os.path.join(subjects_dir, subject.name)))
+            ):
+                for fname in fnames:
+                    if "fsaverage" in fname:
+                        os.rename(
+                            os.path.join(dirpath, fname),
+                            os.path.join(
+                                dirpath, fname.replace("fsaverage", subject.name)
+                            ),
+                        )
+                if "fsaverage" in os.path.basename(dirpath):
+                    new_dirpath = os.path.join(
+                        os.path.dirname(dirpath),
+                        os.path.basename(dirpath).replace("fsaverage", subject.name),
+                    )
+                    os.rename(dirpath, new_dirpath)
+
+        # And then create a meggie coregistration object.
+        params = {}
+        path = subject.coregistration_directory
+        coreg = Coregistration(name, path, params)
+        coreg.save_content()
+        subject.add(coreg, "coregistration")
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         'meggie>=1.3.0',
         'mne>=1.0.0',
         'pyvistaqt',
-        'pysurfer',
+        'nibabel',
         'traitlets'
     ]
 )
